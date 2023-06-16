fc1d6e12b3e32d07a122ccc30094a6d7a774f2e4
Tbone912 <56204801+Tbone912@users.noreply.github.com>
2023-06-16 11:11:57 -0400

    code cleanup

--- src/main/java/com/weather/service/GetData.java
+++ src/main/java/com/weather/service/GetData.java
@@ -2,17 +2,11 @@
 //https://open-meteo.com/en/docs/historical-weather-api
 package com.weather.service;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
 import com.weather.model.WeatherModel;
 
 @Service
@@ -20,12 +14,6 @@ public class GetData {
 
 	public static WeatherModel getData() throws IOException {
 
-		ObjectMapper objectMapper = new ObjectMapper();
-		objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-		objectMapper.setVisibility(
-				VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
-
-		WeatherModel weatherModel = new WeatherModel();
 		RestTemplate restTemplate = new RestTemplate();
 
 		String oikolab = "https://archive-api.open-meteo.com/v1/archive?latitude=33.75&longitude=-84.39&start_date=2023-05-18&end_date=2023-06-01&daily=rain_sum&timezone=auto&precipitation_unit=inch";
